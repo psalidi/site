c84b71a249065f964956291fc01f5c03fabac994
dGrammatiko <d.grammatiko@gmail.com>
2020-07-31 18:24:36 +0200

    a

--- assets_src/js/instagram.js
+++ assets_src/js/instagram.js
@@ -9,7 +9,7 @@ const fetchInstagramPhotos = async (accountUrl) => {
   const json = JSON.parse(response.data.match(instagramRegExp)[1]);
   const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(
     0,
-    8
+    21
   );
   const photos = edges.map(({node}) => {
     return {
